0de212d4cc7d9f24322fc1ac367248c8a65acbaa
trungnl08 <70845041+trungnl08@users.noreply.github.com>
2020-09-21 21:33:11 +0700

    Update DictionaryCommandline.java

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -2,16 +2,15 @@ public class DictionaryCommandline {
     public Dictionary dsnr;
     public DictionaryManagement management;
 
-
+//funtion show word
     public void showAllWords() {
-
         System.out.println("No\tEnglish\tVietnamese");
         for (int i = 0; i < dsnr.getCount(); i++) {
             System.out.println((i + 1) + " " + dsnr.getWorkTarget(i) + " " + dsnr.getWordExplain(i));
         }
     }
 
-
+//use insertFromCommandLine() and showAllWords()
     public void dictionaryBasic() {
         management = new DictionaryManagement(dsnr);
         management.insertFromCommandline();
